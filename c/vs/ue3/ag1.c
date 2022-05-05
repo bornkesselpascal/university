6a11b6502b719374b31d30526b2ccc7596ff2466
Pascal Julian Bornkessel <bornkessel@msn.com>
2022-05-05 16:11:19 +0200

    Blatt 3 AG1 Entwurf

---
+++ c/vs/ue3/ag1.c
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/sem.h>
+#include <stdbool.h>
+
+void reader(size_t process);
+void writer(size_t process);
+
+void init_sem(int sem_num, int sem_init);
+void P(int sem_num);
+void V(int sem_num);
+int getval(int sem_num);
+void setval(int sem_num, int sem_val);
+
+const int sem_writer = 0;
+const int sem_reader = 1;
+const int sem_mutex = 2;
+
+int sem_id;
+
+int main(void)
+{
+    int sem_key;
+    if ((sem_key = ftok("/home/bornk/Dokumente/Developer/university/c/vs/ue3", '1')) < 0)
+    {
+        perror("Fehler bei ftok");
+        exit(1);
+    }
+
+    if ((sem_id = semget(sem_key, 3, IPC_CREAT | 0666)) < 0)
+    {
+        perror("Fehler bei semget");
+        exit(1);
+    }
+
+    // Writer-Semaphore
+    init_sem(sem_writer, 1);
+    // Reader-Semaphore
+    init_sem(sem_reader, 5);
+    // Mutex-Semaphore
+    init_sem(sem_mutex, 1);
+
+    // Erstellung der SCHREIBER
+    for (size_t process = 0; process < 2; process++)
+    {
+        switch (fork())
+        {
+        case -1:
+            perror("Erzeugung fehlgeschlagen");
+            exit(1);
+
+        case 0:
+            // Kindprozess
+            printf("Prozess: %ld\tPID: %d\n", process, getpid());
+            writer(process);
+            exit(0);
+
+        default:
+            // Vaterprozess
+            break;
+        }
+    }
+
+    // Erstellung der LESER
+    for (size_t process = 0; process < 5; process++)
+    {
+        switch (fork())
+        {
+        case -1:
+            perror("Erzeugung fehlgeschlagen");
+            exit(1);
+
+        case 0:
+            // Kindprozess
+            printf("Prozess: %ld\tPID: %d\n", process, getpid());
+            reader(process);
+            exit(0);
+
+        default:
+            // Vaterprozess
+            break;
+        }
+    }
+}
+
+void reader(size_t process)
+{
+    while (true)
+    {
+        // Zugriff auf Variable
+        P(sem_mutex);
+        {
+            P(sem_reader);
+            if (4 == getval(sem_reader))
+            {
+                P(sem_writer);
+            }
+        }
+        V(sem_mutex);
+
+        {
+            // READ
+        }
+
+        P(sem_mutex);
+        {
+            V(sem_reader);
+            if (5 == getval(sem_reader))
+            {
+                V(sem_writer);
+            }
+        }
+        V(sem_mutex);
+
+        sleep(1);
+
+
+        // Unkritischer Bereich
+        printf("Unkritischer Bereich TBC\n");
+        sleep(1);
+    }
+}
+
+void writer(size_t process)
+{
+    while (true)
+    {
+        // Zugriff auf Variable
+        P(sem_writer);
+
+        {
+            // WRITE
+        }
+
+        V(sem_writer);
+
+        sleep(1);
+
+
+        // Unkritischer Bereich
+        printf("Unkritischer Bereich TBC\n");
+        sleep(1);
+    }
+}
+
+void init_sem(int sem_num, int sem_init)
+{
+    if (semctl(sem_id, sem_num, SETVAL, sem_init) < 0)
+    {
+        perror("Fehler bei semctl");
+        exit(1);
+    }
+}
+
+void P(int sem_num)
+{
+    struct sembuf semaphore;
+
+    semaphore.sem_num = sem_num;
+    semaphore.sem_op = -1;
+    semaphore.sem_flg = ~(IPC_NOWAIT | SEM_UNDO);
+
+    if (semop(sem_id, &semaphore, 1))
+    {
+        perror("Fehler bei semop P()");
+        exit(1);
+    }
+}
+
+void V(int sem_num)
+{
+    struct sembuf semaphore;
+
+    semaphore.sem_num = sem_num;
+    semaphore.sem_op = 1;
+    semaphore.sem_flg = ~(IPC_NOWAIT | SEM_UNDO);
+
+    if (semop(sem_id, &semaphore, 1))
+    {
+        perror("Fehler bei semop V()");
+        exit(1);
+    }
+}
+
+int getval(int sem_num)
+{
+    int semval = semctl(sem_id, sem_num, GETVAL, 0);
+
+    if (semval < 0)
+    {
+        perror("Fehler bei semctl");
+        exit(1);
+    }
+
+    return semval;
+}
+
+void setval(int sem_num, int sem_val)
+{
+    init_sem(sem_num, sem_val);
+}
