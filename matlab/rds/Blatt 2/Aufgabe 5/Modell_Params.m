1f1e18ed6dec3924f610781bf5f827e74fda4c88
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-05-19 12:32:50 +0200

    RDS - Blatt 2, Aufgabe 5

---
+++ matlab/rds/Blatt 2/Aufgabe 5/Modell_Params.m
@@ -0,0 +1,9 @@
+% Praktikum Regelungstechnik
+% Pascal Julian Bornkessel, FFI 6
+% Blatt 2, Aufgabe 5
+
+Ra = 3;         % in Ohm; Ankerwiderstand kl. Maschine
+La = 1.5e-3;    % in H; Ankerinduktivität
+Cphi = 0.02;    % in Vs; Drehmomentkonstante
+J = 10e-6;      % in kg/m^2; Massenträgheitsmoment
+Ua = 12;        % Ankerspannung
